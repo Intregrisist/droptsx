4eb8587dcafd3fb9e5cc58b17beb8666edfe2888
Jed Mao <jedmao@outlook.com>
2018-04-08 23:24:07 -0500

    Introduce basic Button features

--- src/components/b/Button.test.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Button from './Button';
-
-it('renders without crashing', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<Button />, div);
-});
---
+++ src/exports.ts
@@ -0,0 +1,2 @@
+// Elements
+export { default as Button } from './elements/Button'
